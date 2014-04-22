6ec936a5c7337bc4ed8d5381b6c2db39f0b8262e
waratte <admin@bb.waratteka.net>
2014-04-22 03:08:34 -0400

    Sockets, threading, multiple servers, config file
    Additions:
    ------------
    - I added support for multithreading
    - I added support for multiple servers
    - I added code to load and use the config file
    - Replaced the irclib with sockets (Sorry frank, but it would have taken
    longer to understand how that library worked also, it would make the
    code more OOP-oriented)
    
    Things to look out for:
    -------------------------
    - PINGing might not work, will look to fix that in my next revision if
    it needs to be
    - No easy way to close out of the bot, not even a keyboard exception
    (Ctrl + Z) will terminate the bot. Threads are running independent of
    the main thread, so I'll soon implement something to solve that.
    - No comments :P I'll add some soon
    
    Things to add:
    ----------------
    
    - Support for plugins and commands, we need them.
    - SASL and other forms of authentication
    - Official, "tried and true" way of daemonizing bot

--- bot.py
+++ bot.py
@@ -1,26 +1,67 @@
-import sys
-import irc.client
+import sys 
+import socket
+import threading
+import re
 
-prefix = "@"
+def socketSend( Socket, String ):
+    "coverts a string to send to a socket"
+    
+    Socket.send( String.encode() )
+    
+class ServerThreads (threading.Thread):
+    def __init__(self, Config, Server, Channels):
+        threading.Thread.__init__(self)
+        self.Config = Config
+        self.Server = Server
+        self.Channels = Channels
+        
+    def run(self):
+        "template for IRC server loop"
+    
+        Socket = socket.socket()
+        
+        print("Connecting to %s at port %i..." % (self.Server["address"], self.Server["port"]) )
+        Socket.connect((self.Server["address"], self.Server["port"]))
+        SocketAsFile = Socket.makefile()
+            
+        while True:
+            Line = SocketAsFile.readline()
+            if Line.find("376"):
+                break
+                
+            else:
+                print( '[%s] %s' % (self.Server["name"], Line) )
 
-def on_connect(connection, event):
-    connection.join("#amagital")
+        socketSend(Socket, 'NICK %s\r\n' % self.Server["identity"]["name"])
+        print( "[BOT] " + "NICK %s" % self.Server["identity"]["name"] )
+        
+        socketSend(Socket, 'USER %s %s bla :%s\r\n' % (self.Server["identity"]["ident"], 
+                                                       self.Server["identity"]["hostname"], 
+                                                       self.Server["identity"]["realname"]))
+                                                  
+        print("[BOT] " + "USER %s %s bla :%s" % (self.Server["identity"]["ident"], 
+                                                   self.Server["identity"]["hostname"], 
+                                                   self.Server["identity"]["realname"]))
 
-def on_pub_msg(connection, event):
-    if event.arguments[0][0:1] == prefix :
-        on_command(connection, event.target, event.arguments[0][1:])
+        for Channel in self.Channels:
+            socketSend(Socket, 'JOIN :%s\r\n' % Channel["name"])
+            print("[BOT] " + "JOIN :%s" % Channel["name"])
+            
+            socketSend(Socket, 'PRIVMSG %s :%s\r\n' % (Channel["name"], "I got 99 problems but connecting to IRC ain't one."))
+            print("[BOT] " + "PRIVMSG %s :%s" % (Channel["name"], "I got 99 problems but connecting to IRC ain't one."))
 
-def on_command(connection, target, command):
-    if command[0:5] == "echo " :
-        connection.privmsg(target, command[5:])
-
-client = irc.client.IRC()
-try: 
-    con = client.server().connect("irc.freenode.net", 6667, "bestbotever")
-except irc.client.ServerConnectionError:
-    print(sys.exc_info()[1])
-    sys.exit(1)
-con.add_global_handler("welcome", on_connect)
-con.add_global_handler("pubmsg", on_pub_msg)
-
-client.process_forever()
+        while True: #Max size of an IRC message (512) * the amount of bytes in a unicode string (4)
+            Line = SocketAsFile.readline()
+            
+            if not Line:
+                break
+            
+            MatchedObject = re.match(r'^PING (.*?)$', Line)
+            if MatchedObject:
+                socketSend(Socket, 'PONG %s\r\n' % MatchedObject.group())
+                print("[BOT] " + "PONG %s" % MatchedObject.group())
+                
+            else:
+                print( '[%s] %s' % (self.Server["name"], Line) )
+                
+        thread.exit()
