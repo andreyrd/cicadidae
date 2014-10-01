c7267b852b42394af549b92de26c085d31a453bb
Andrey Radchishin <andreyrd1@hotmail.com>
2014-10-01 08:27:07 -0700

    Never mind about Java

---
+++ cicadidae/bot.py
@@ -0,0 +1,71 @@
+import sys 
+import socket
+import threading
+import time
+import re
+
+from include import socketSend
+
+from modules.Utilities import plugin
+    
+class ServerThreads (threading.Thread):
+    def __init__(self, Config, Server, Channels):
+        threading.Thread.__init__(self)
+        self.Config = Config
+        self.Server = Server
+        self.Channels = Channels
+        
+    def run(self):
+        "template for IRC server loop"
+    
+        Socket = socket.socket()
+        
+        print("Connecting to %s at port %i..." % (self.Server["address"], self.Server["port"]) )
+        Socket.connect((self.Server["address"], self.Server["port"]))
+        SocketAsFile = Socket.makefile()
+            
+        while True:
+            Line = SocketAsFile.readline()
+            if Line.find("376"):
+                break
+                
+            else:
+                print( '[%s] %s' % (self.Server["name"], Line) )
+
+        socketSend(Socket, 'NICK %s\r\n' % self.Server["identity"]["name"])
+        print( "[BOT] " + "NICK %s" % self.Server["identity"]["name"] )
+        
+        socketSend(Socket, 'USER %s %s bla :%s\r\n' % (self.Server["identity"]["ident"], 
+                                                       self.Server["identity"]["hostname"], 
+                                                       self.Server["identity"]["realname"]))
+                                                  
+        print("[BOT] " + "USER %s %s bla :%s" % (self.Server["identity"]["ident"], 
+                                                   self.Server["identity"]["hostname"], 
+                                                   self.Server["identity"]["realname"]))
+
+        for Channel in self.Channels:
+            socketSend(Socket, 'JOIN :%s\r\n' % Channel["name"])
+            print("[BOT] " + "JOIN :%s" % Channel["name"])
+            
+            socketSend(Socket, 'PRIVMSG %s :%s\r\n' % (Channel["name"], "I got 99 problems but connecting to IRC ain't one."))
+            print("[BOT] " + "PRIVMSG %s :%s" % (Channel["name"], "I got 99 problems but connecting to IRC ain't one."))
+
+        while True: #Max size of an IRC message (512) * the amount of bytes in a unicode string (4)
+            time.sleep(0.1)
+        
+            Line = SocketAsFile.readline()
+            
+            if not Line:
+                break
+            
+            MatchedObject = re.match(r'^PING (.*?)$', Line)
+            if MatchedObject:
+                socketSend(Socket, 'PONG %s\r\n' % MatchedObject.group())
+                print("[BOT] " + "PONG %s" % MatchedObject.group())
+            
+            MatchedObject2 = re.search(r'die', Line)
+            if MatchedObject2:
+                modules.Utilities.plugin.die('Fine, meanie >:(')
+                
+            else:
+                print( '[%s] %s' % (self.Server["name"], Line) )
---
+++ main.py
@@ -0,0 +1,18 @@
+from cicadidae import bot
+from cicadidae import include
+import json
+
+ConfigFile = open( "config.json", "r+" )
+
+print("Loading config file...")
+
+Config = json.load( ConfigFile )  
+
+print("Decoding config file into python object...")
+
+ConfigFile.close()
+
+ThreadList = []
+for Server in Config["servers"]:
+    ThreadList.append(bot.ServerThreads( Config, Config["servers"][Server], Config["channels"][Server] ))
+    (max(ThreadList)).start()
